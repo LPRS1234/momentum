71b659020c4f0768acea61f0f0fbb309c55269b5
LPRS1234 <107792755+LPRS1234@users.noreply.github.com>
2025-01-05 22:48:58 +0900

    CSS

--- js/background.js
+++ js/background.js
@@ -6,8 +6,6 @@ const images = [
 
 const chosenImage = images[Math.floor(Math.random() * images.length)]
 
-const bgImage = document.createElement("img");
+const bgImage = `url(img/${chosenImage})`
 
-bgImage.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
+document.body.style.backgroundImage = bgImage;
